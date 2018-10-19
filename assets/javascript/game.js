bacc0f8c3e89866310fa6496adf65d186166157f
Koxinga <timwu1015@gmail.com>
2018-10-19 16:01:48 -0400

    initial push

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+    var kratos = {
+        health: 180,
+        attack: 7,
+        enemyAttackBack: 25,  
+    };
+
+    var atreus = {
+        health: 120,
+        attack: 8,
+        enemyAttackBack: 15,  
+    };
+
+    var baldur = {
+        health: 150,
+        attack: 8,
+        enemyAttackBack: 20,  
+    };
+
+    var thor = {
+        health: 100,
+        attack: 14,
+        enemyAttackBack: 5,  
+    };
+
+    var increment = 6;
+    var character = "";
+    var enemy = "";
+    var charChosen = false;
+    
+    $(".pic").on("click", function() {
+        if(!charChosen) {
+            character = $(this).alt();
+            $(".character").clone().appendTo("#pick");
+            charChosen = true;
+        }
+        else {
+            enemy = $(this).alt();
+            $(".enemy").clone().appendTo("#enemy");
+        }
+    });
+
+});
